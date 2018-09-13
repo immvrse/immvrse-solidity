cdc84d05d27153b2c9f649141014ee9bc1cb6f49
immvrse <35700753+immvrse@users.noreply.github.com>
2018-09-13 16:29:38 +0100

    Update ERC20.sol

--- contracts/token/ERC20.sol
+++ contracts/token/ERC20.sol
@@ -1,7 +1,7 @@
 pragma solidity 0.4.24;
 
-import "github.com/OpenZeppelin/openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
-import "github.com/OpenZeppelin/openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "/OpenZeppelin/openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
+import "/OpenZeppelin/openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 
 /**
