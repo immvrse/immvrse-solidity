ac12df4e2685f1dae42c6ff7ce4ba694d3434878
immvrse <35700753+immvrse@users.noreply.github.com>
2018-10-15 18:44:20 +0100

    Update and rename chainlist.sol to productlist.sol

--- contracts/Marketplace/chainlist/contracts/productlist.sol
+++ contracts/Marketplace/chainlist/contracts/productlist.sol
@@ -1,13 +1,13 @@
 pragma solidity ^0.4.24;
 
-contract chainlist {
+contract productlist {
   //state variables
   address seller;
   string name;
   string description;
   uint256 price;
 
-  //sell an article
+  //sell an product
   function sellarticle(string _name, string _description, uint256 _price) public{
     seller = msg.sender;
     name = _name;
@@ -15,7 +15,7 @@ contract chainlist {
     price = _price;
   }
 
-  //get an sellarticle
+  //get an sellproduct
   function getarticle() public view returns (address _seller, string _name, string _description, uint256 _price)
   {
     return(seller, name, description, price);
