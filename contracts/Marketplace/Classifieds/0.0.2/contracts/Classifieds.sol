fcb02b7fd4aa15cad2f80fa411032d4d6fde444b
Shyeem Rahman <43682009+sharod13@users.noreply.github.com>
2018-11-09 14:55:13 +0600

    Add files via upload

---
+++ contracts/Marketplace/Classifieds/0.0.2/contracts/Classifieds.sol
@@ -0,0 +1,68 @@
+pragma solidity ^0.4.24;
+
+contract Classifieds {
+  //state variables
+  address seller;
+  address buyer;
+  string name;
+  string description;
+  uint256 price;
+
+  //events
+  event LogSellArticle(
+    address indexed _seller,
+    string _name,
+    uint256 _price
+    );
+
+    event LogBuyArticle(
+      address indexed _seller,
+      address indexed _buyer,
+      string _name,
+      uint _price
+      );
+
+
+  //sell an article
+  function sellArticle(string _name, string _description, uint256 _price) public{
+    seller = msg.sender;
+    name = _name;
+    description = _description;
+    price = _price;
+
+    LogSellArticle(seller, name, price);
+  }
+
+
+
+  //get an article
+  function getArticle() public view returns (address _seller, string _name, string _description, uint256 _price)
+  {
+    return(seller, name, description, price);
+  }
+
+ //buy an article
+
+ function buyArticle() payable public{
+ //we can check whether there is an article for sale
+    require(seller != 0x0);
+
+    //we check whether the article has not been sold yet
+    require(buyer == 0x0);
+
+    //we dont allow the seller to buy his own article
+    require (msg.sender != seller);
+
+  // we check the value sent corresponds to the price of the article
+  require(msg.value == price);
+
+  //keep buyers information
+  buyer = msg.sender;
+
+    // the buyer can pay the seller
+    seller.transfer(msg.value);
+
+    // trigger the events
+    LogBuyArticle(seller,buyer,name,price);
+  }
+}
